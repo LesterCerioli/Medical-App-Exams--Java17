701c88098e0d4f2a94d0dccae12a5d7f7f452e9e
LesterCerioli <lesterlucasit@hotmail.com>
2026-02-15 08:35:38 -0500

    Initial repository

---
+++ src/main/java/com/medical/app/exams/ExamsApplication.java
@@ -0,0 +1,13 @@
+package com.medical.app.exams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamsApplication.class, args);
+	}
+
+}
